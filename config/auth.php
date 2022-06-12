f9644c56570855ad2d9c01e2f67e2deb463c9aa4
wisdomntui <wisdom.ntui@gmail.com>
2022-06-12 13:00:47 +0100

    Install laravel passport and include additional guards for authentication

--- config/auth.php
+++ config/auth.php
@@ -11,7 +11,7 @@ return [
     | reset options for your application. You may change these defaults
     | as required, but they're a perfect start for most applications.
     |
-    */
+     */
 
     'defaults' => [
         'guard' => 'web',
@@ -33,12 +33,24 @@ return [
     |
     | Supported: "session"
     |
-    */
+     */
 
     'guards' => [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+        'admin-web-api' => [
+            'driver' => 'passport',
+            'provider' => 'admins',
+        ],
+        'super-web-api' => [
+            'driver' => 'passport',
+            'provider' => 'superadmins',
+        ],
+        'mobile-api' => [
+            'driver' => 'passport',
+            'provider' => 'members',
         ],
     ],
 
@@ -57,12 +69,24 @@ return [
     |
     | Supported: "database", "eloquent"
     |
-    */
+     */
 
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
+        'superadmins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\SuperAdmin::class,
+        ],
+        'members' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Member::class,
         ],
 
         // 'users' => [
@@ -84,7 +108,7 @@ return [
     | considered valid. This security feature keeps tokens short-lived so
     | they have less time to be guessed. You may change this as needed.
     |
-    */
+     */
 
     'passwords' => [
         'users' => [
@@ -104,7 +128,7 @@ return [
     | times out and the user is prompted to re-enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
     |
-    */
+     */
 
     'password_timeout' => 10800,
 
