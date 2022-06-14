11fef5861eedeb5d02d83cf917fb906da2930c6f
wisdomntui <wisdom.ntui@gmail.com>
2022-06-14 22:33:37 +0100

    Revert route file and Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\CsvTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Request;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, CsvTrait;
-
-    public function index(Request $request)
-    {
-        return $this->setCSVStructure(['Name', 'Email', 'Number'], 'wisdom-cooperative');
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', [App\Http\Controllers\Controller::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
