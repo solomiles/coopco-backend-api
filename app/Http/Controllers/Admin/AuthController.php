7e4205b30f76a3d7949ad53264d7b0e68b5ea8aa
Gabriel Osuobiem <osuobiem@gmail.com>
2022-06-21 00:44:19 +0100

    add admin login method

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -3,9 +3,69 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Process login action
+     * @param Request $request - Request object
+     * 
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response([
+                'status' => 400,
+                'errors' => $validator->errors()->messages()
+            ], 400);
+        }
+
+        $invalidCredentialsResponse = [
+            'status' => 401,
+            'message' => 'Invalid Credentials'
+        ];
+
+        $username = $request->username;
+        $password = $request->password;
+
+        $admin = Admin::where('username', $username)->first();
+
+        if (!$admin) {
+            return response()->json($invalidCredentialsResponse, 401);
+        }
+
+        if (!Hash::check($password, $admin->password)) {
+            return response()->json($invalidCredentialsResponse, 401);
+        }
+
+        $token = $admin->createToken('Cooperative Admin Token');
+       
+        $data = [
+            'admin' => $admin,
+            'token' => $token->accessToken,
+            'token_type' => 'Bearer',
+            'token_expires' => Carbon::parse(
+                $token->token->expires_at
+            )->toDateTimeString(),
+        ];
+
+        return response([
+            'status' => 200,
+            'message' => 'Login Successful',
+            'data' => $data
+        ], 200);
+    }
 }
+
+?>
