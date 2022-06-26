44552d8948fa5c4e89f84db40c1ae3b57becdc64
wisdomntui <wisdom.ntui@gmail.com>
2022-06-26 15:04:32 +0100

    Modify CSV Trait to accept uploaded file request pointer object & revert Controller.php and web.php to original state

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,27 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\CsvTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Request;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, CsvTrait;
-
-    public function index(Request $request)
-    {
-        // $file = public_path('storage/csv-templates/wisdom-cooperative.csv');
-
-        // return $this->setCSVStructure(['Name', 'Email', 'Number'], 'wisdom-cooperative');
-        // $rules = [
-        //     'Name' => 'required|regex:/^[\pL\s]+$/u',
-        //     'Email' => 'required|email',
-        //     'Number' => 'required|numeric|digits:11',
-        // ];
-        // dd($this->validateCSVFile($rules, $file));
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
