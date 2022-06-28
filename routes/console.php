5aa6ed32dd811c3980e304e7943b32c56fd5f3e5
Gabriel Osuobiem <osuobiem@gmail.com>
2022-06-28 11:50:44 +0100

    add schema switch artisan command

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
 
 /*
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Switch psql db schema
+Artisan::command('schema:switch {schemaName?}', function(Request $request, $schemaName = 'main') {
+    createSchema($schemaName);
+    switchSchema($request, $schemaName);
+    
+    echo "- - - - - - - - - - - - - -\n";
+    echo " DB Schema switched to * $schemaName * \n";
+    echo "- - - - - - - - - - - - - -";
+})->purpose('Switch psql db schema');
