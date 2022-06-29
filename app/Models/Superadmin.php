527e5e128c7b9e0cd1ae95d4eb212ec3d5c75633
Gabriel Osuobiem <osuobiem@gmail.com>
2022-06-29 23:45:44 +0100

    add superadmin migration and model

--- app/Models/Superadmin.php
+++ app/Models/Superadmin.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Superadmin extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $hidden = [
+        'password', 'remember_token', 'created_at', 'updated_at', 'deleted_at'
+    ];
 }
