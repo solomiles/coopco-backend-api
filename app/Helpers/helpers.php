d6e6cdb6d32cf49afb0ff35769362f4aeb8b4dfe
wisdomntui <wisdom.ntui@gmail.com>
2022-07-06 12:10:43 +0100

    Install and test crazybooot/base64-validation validator library

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * Swicth psql DB schema
@@ -10,10 +13,11 @@ use Illuminate\Support\Facades\DB;
  *
  * @return void
  */
-function switchSchema($request, $schema = null) {
-	$schema = $schema ? $schema : $request->getHttpHost();
+function switchSchema($request, $schema = null)
+{
+    $schema = $schema ? $schema : $request->getHttpHost();
 
-	config(['database.connections.pgsql.search_path' => $schema]);
+    config(['database.connections.pgsql.search_path' => $schema]);
     DB::purge();
 }
 
@@ -24,8 +28,9 @@ function switchSchema($request, $schema = null) {
  *
  * @return void
  */
-function setEmailCredentials($emailConfig) {
-    config(['mail.mailers.'.$emailConfig['mailer'] => [
+function setEmailCredentials($emailConfig)
+{
+    config(['mail.mailers.' . $emailConfig['mailer'] => [
         'transport' => $emailConfig['mailer'],
         'host' => $emailConfig['host'],
         'port' => $emailConfig['port'],
@@ -43,10 +48,11 @@ function setEmailCredentials($emailConfig) {
 
 /**
  * Random password generator
- * 
+ *
  * @return string
  */
-function randomPassword() {
+function randomPassword()
+{
     $alphaNumChar = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&()<,>?';
     return substr(str_shuffle($alphaNumChar), 0, 8);
 }
@@ -54,19 +60,50 @@ function randomPassword() {
 /**
  * Global variable accessor helper
  * @param string $constant - Constant key in config/global.php
- * 
+ *
  * @return string
  */
-function g($constant) {
-    return config('global.'.$constant);
+function g($constant)
+{
+    return config('global.' . $constant);
 }
 
 /**
  * Create schema if it doesn't exist
  * @param string $schemaName
- * 
+ *
  * @return void
  */
-function createSchema($schemaName) {
-    DB::unprepared('CREATE SCHEMA IF NOT EXISTS '.$schemaName);
+function createSchema($schemaName)
+{
+    DB::unprepared('CREATE SCHEMA IF NOT EXISTS ' . $schemaName);
+}
+
+/**
+ * Convert base64 encoded file to actual file
+ * @param string $base64File
+ *
+ * @return Illuminate\Http\UploadedFile
+ */
+function base64ToFile($base64File)
+{
+    // decode the base64 file
+    $fileData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64File));
+
+    // save it to temporary dir first.
+    $tmpFilePath = sys_get_temp_dir() . '/' . Str::uuid()->toString();
+    file_put_contents($tmpFilePath, $fileData);
+
+    // this just to help us get file info.
+    $tmpFile = new File($tmpFilePath);
+
+    $file = new UploadedFile(
+        $tmpFile->getPathname(),
+        $tmpFile->getFilename(),
+        $tmpFile->getMimeType(),
+        0,
+        true// Mark it as test, since the file isn't from real HTTP POST.
+    );
+
+    return $file;
 }
