b13ea261b467e40c22347676fb925950f5738f96
Gabriel Osuobiem <osuobiem@gmail.com>
2022-07-13 12:28:24 +0100

    update schema migration path

--- routes/console.php
+++ routes/console.php
@@ -52,7 +52,7 @@ Artisan::command('schema:migrate {schemaName}', function(Request $request, $sche
     echo "\n    Migrating $schemaName schema . . .\n";
 
     try {
-        Artisan::call('migrate');
+        Artisan::call('migrate', ['--path' => base_path('database/migrations/'.$schemaName)]);
 
         $files = array_diff(scandir(base_path('database/migrations/'.$schemaName)), array('.', '..'));
 
