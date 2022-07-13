dafa5177fdc8609b8959cbb11e3e3ed113de80ec
wisdomntui <wisdom.ntui@gmail.com>
2022-07-13 09:44:51 +0100

    Add auth middleware to member API

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
-class Member extends Model
+class Member extends Authenticatable
 {
     use HasFactory, SoftDeletes, HasApiTokens;
 
