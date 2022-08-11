666d7e668c722277036d249d220a7efed9d0d614
Gabriel Osuobiem <osuobiem@gmail.com>
2022-08-11 11:58:41 +0100

    fix member login bug

--- app/Http/Controllers/Member/AuthController.php
+++ app/Http/Controllers/Member/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|email:filter,rfc,dns',
+            'email' => 'required|email:filter,dns',
             'password' => 'required',
         ]);
 
