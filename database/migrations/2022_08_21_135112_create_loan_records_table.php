89c891e7ddf375d1dfc4f782deee69003e8f6574
Gabriel Osuobiem <osuobiem@gmail.com>
2022-08-21 15:31:50 +0100

    add loan records table

---
+++ database/migrations/2022_08_21_135112_create_loan_records_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_records', function (Blueprint $table) {
+            $table->id();
+            $table->double('balance');
+            $table->double('amount')->default(0);
+            $table->double('interest')->default(0);
+            $table->double('accumulated_interest')->default(0);
+            $table->int('month');
+            $table->int('year');
+            $table->boolean('exempted')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_records');
+    }
+};
