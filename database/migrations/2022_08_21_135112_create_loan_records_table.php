58ec59503ac92137042a6543a317c9ed3fd5f166
Gabriel Osuobiem <osuobiem@gmail.com>
2022-08-28 23:14:57 +0100

    update loan_records migration structure

--- database/migrations/2022_08_21_135112_create_loan_records_table.php
+++ database/migrations/2022_08_21_135112_create_loan_records_table.php
@@ -15,13 +15,12 @@ return new class extends Migration
     {
         Schema::create('loan_records', function (Blueprint $table) {
             $table->id();
-            $table->double('balance');
             $table->double('amount')->default(0);
             $table->double('interest')->default(0);
             $table->double('accumulated_interest')->default(0);
             $table->integer('month');
             $table->integer('year');
-            $table->boolean('exempted')->default(false);
+            $table->foreignId('loan_id')->constrained('loans')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
